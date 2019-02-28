0a37c445780c3495b41c2192b96e24b9410b8489
Michael John Klopfer <mklopfer@uci.edu>
2019-02-28 11:41:30 -0800

    Update CRC Example Code

--- libraryfiles/SPI_LIBRARY/ADE9078/ADE9078.cpp
+++ libraryfiles/SPI_LIBRARY/ADE9078/ADE9078.cpp
@@ -717,26 +717,23 @@ unsigned short crc16(char data_p, unsigned short length){ //example CCITT 16 CRC
    #define POLY 0x8408
 	
    crc = 0xffff;
-	
-   if (length == 0)
-		  return (~crc);
-	
-   do {
-		  for (i = 0; i < 8; i++) 
-		  {
-			  data = (unsigned int)0xff & data_p++; 
-			  data >>= 1;
-				if ((crc & 0x0001) ^ (data & 0x0001))
-					   crc = (crc >> 1) ^ POLY;
-				else
-					   crc >>= 1;
-		  }
-   } while (--length);
-	
-   crc = ~crc;
-	
-   data = crc;
-   crc = (crc << 8) | (data >> 8 & 0xFF);
-	
-   return (crc);
+	       if (length == 0)
+              return (~crc);
+        
+       do {
+              for (i = 0, data = (unsigned int)0xff & data_p++; i < 8; i++, data >>= 1) //alternative notation for nested for loops
+			  {
+                    if ((crc & 0x0001) ^ (data & 0x0001))
+                           crc = (crc >> 1) ^ POLY;
+                    else
+                           crc >>= 1;
+              }
+       } while (--length);
+        
+       crc = ~crc;
+        
+       data = crc;
+       crc = (crc << 8) | (data >> 8 & 0xFF);
+        
+       return (crc);
 }
