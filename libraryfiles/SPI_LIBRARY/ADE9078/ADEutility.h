f118a4da530569fa509853236ba09451b66f8051
Jacky <jwan1@uci.edu>
2019-03-11 18:15:36 -0700

    Added new algorithm from source found - Jacky

--- libraryfiles/SPI_LIBRARY/ADE9078/ADEutility.h
+++ libraryfiles/SPI_LIBRARY/ADE9078/ADEutility.h
@@ -79,7 +79,26 @@ uint16_t crc16(unsigned char* data_p, uint16_t length){ //example CCITT 16 CRC f
 
        return (crc);
 }
+/* New Algorithm From https://stackoverflow.com/questions/21252069/ccitt-crc-16-bit-start-value-0xffff/21263265#21263265
+CRC Calculator: https://www.lammertbies.nl/comm/info/crc-calculation.html
+Decimal to Hex Converter: https://cryptii.com/pipes/integer-encoder
 
+uint16_t crc16(unsigned char* data_p, uint16_t length)
+{
+    unsigned char i;
+    uint16_t data;
+    uint16_t crc = 0xffff;
+
+ if (length == 0)
+    return crc;
+while (length--) {
+    crc ^= *(unsigned char *)data_p++ << 8;
+    for (i=0; i < 8; i++)
+        crc = crc & 0x8000 ? (crc << 1) ^ 0x1021 : crc << 1;
+}
+return crc & 0xffff;
+}
+/*
 
 // Crc algorithm supplied by jacky. Not compiling
 /*
